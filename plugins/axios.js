594ef46a96dd480bb49bc6708b09db51e58fd41c
Balazs Szabo <balazs.szabo@gmail.com>
2018-06-04 10:25:38 +0300

    axios plugin off

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,8 +2,8 @@ import * as axios from 'axios'
 
 let options = {}
 // The server-side needs a full url to works
-if (process.server) {
-  options.baseURL = `http://localhost:${process.env.PORT || 3000}`
-}
+// if (process.server) {
+//   options.baseURL = `http://localhost:${process.env.PORT || 3000}`
+// }
 
 export default axios.create(options)
